9e9d52f1b336df15c4ec0a832dcd02d8f5ba014f
jeswanth <jachu.k1212@gmail.com>
2024-11-14 17:00:01 +0530

    Router_Klinik_Edited

--- klinik/src/Components/Header.jsx
+++ klinik/src/Components/Header.jsx
@@ -75,55 +75,16 @@ const Header = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <div className="navbar-nav ms-auto p-4 p-lg-0">
-            {/* <a href="index.html" className="nav-item nav-link active">
-              Home
-            </a> */}
+            
             <Link to={''} className="nav-item nav-link">Home</Link>
-            {/* <a href="about.html" className="nav-item nav-link">
-              About
-            </a> */}
+            
             <Link to={'about'} className="nav-item nav-link">About</Link>
-            {/* <a href="service.html" className="nav-item nav-link">
-              Service
-            </a> */}
+            
             <Link to={'services'} className="nav-item nav-link">Services</Link>
-            {/* <div className="nav-item dropdown">
-              <a
-                href="#"
-                className="nav-link dropdown-toggle"
-                data-bs-toggle="dropdown"
-              >
-                Pages
-              </a>
-              <div className="dropdown-menu rounded-0 rounded-bottom m-0">
-                <a href="feature.html" className="dropdown-item">
-                  Feature
-                </a>
-                <a href="team.html" className="dropdown-item">
-                  Our Doctor
-                </a>
-                <a href="appointment.html" className="dropdown-item">
-                  Appointment
-                </a>
-                <a href="testimonial.html" className="dropdown-item">
-                  Testimonial
-                </a>
-                <a href="404.html" className="dropdown-item">
-                  404 Page
-                </a>
-              </div>
-            </div> */}
-            {/* <a href="contact.html" className="nav-item nav-link">
-              Contact
-            </a> */}
+            
             <Link to={'contact'} className="nav-item nav-link">Contact</Link>
           </div>
-          {/* <a
-            href=""
-            className="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block"
-          >
-            Appointment<i className="fa fa-arrow-right ms-3"></i>
-          </a> */}
+          
           <Link to={'appointment'} className="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Appointment <i className="fa fa-arrow-right ms-3"></i></Link>
         </div>
       </nav>
